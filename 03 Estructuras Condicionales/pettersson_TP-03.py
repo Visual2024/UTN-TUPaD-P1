3f0373ac8418db7a99c2baf7ac631e984c7f3f1b
Facundo Pettersson <visualstudio.creator23@gmail.com>
2025-04-01 23:09:01 -0300

    fix: :bug: optimizing the code

--- 03 Estructuras Condicionales/pettersson_TP-03.py
+++ 03 Estructuras Condicionales/pettersson_TP-03.py
@@ -63,9 +63,9 @@ else:
 # de la función len() en Python para evaluar la cantidad de elementos que tiene un iterable tal
 # como una lista o un string.
 
-contraseña = input("Ingrese su contraseña: ")
+contrasena = input("Ingrese su contraseña: ")
 
-if len(contraseña) >= 8 and len(contraseña) <= 14:
+if 8 <= len(contrasena) <= 14:
     print("Ha ingresado una contraseña correcta")
 else:
     print("Por favor, ingrese una contraseña de entre 8 y 14 caracteres")
