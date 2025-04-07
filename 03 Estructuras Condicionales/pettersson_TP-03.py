88939fe85e0f96edf3c996f031cd65356a2894ff
Facundo Pettersson <visualstudio.creator23@gmail.com>
2025-04-07 20:32:16 -0300

    feat changes

--- 03 Estructuras Condicionales/pettersson_TP-03.py
+++ 03 Estructuras Condicionales/pettersson_TP-03.py
@@ -197,9 +197,9 @@ if hemisferio == "N":
 
 if hemisferio == "S":
       if mes == "diciembre"  and dia >= 21 or mes == "enero" or mes == "febrero" or mes == "marzo" and dia <= 20:
-            print("Verano")
+            print("verano")
       elif mes == "marzo" and dia >= 21 or mes == "abril" or mes == "mayo" or mes == "junio" and dia <= 20:
-            print("Otoño")
+            print("otoño")
       elif mes == "junio" and dia >= 21 or mes == "julio" or mes == "agosto" or mes == "septiembre" and dia <= 20:
                 print("Invierno")
       elif mes == "septiembre" and dia >= 21 or mes == "octubre" or mes == "noviembre" or mes == "diciembre" and dia <= 20:
